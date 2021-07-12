4d0531ecca19ef3af8b8d82c80088c5b3f8bd8ac
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2021-07-12 15:42:44 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
 	"github.com/stjudecloud/cgc-go/cmd"
 )
 
 var cgcURL string = ""
 
 func main() {
+	logrus.SetLevel(logrus.DebugLevel)
 	cmd.Execute()
 }
